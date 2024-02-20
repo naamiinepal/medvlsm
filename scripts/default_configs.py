26d5841e673d8df26ce8e0a380150ee3f67d94c8
Manish Dhakal <074bct521.manish@pcampus.edu.np>
2024-02-20 17:46:13 +0545

    refactor: typos in cfgs updated

--- scripts/default_configs.py
+++ scripts/default_configs.py
@@ -9,37 +9,35 @@ devices = [0]
 precision = "16-mixed"
 debugger = False
 models = ["clipseg", "cris", "biomed_clipseg", "biomed_clipseg_d"]
-freeze_encoder = False
 
 models_configs = {
-    "clipseg": {"batch_size": 128, "lr": 0.002},
-    "clipseg_adapter": {"batch_size": 128, "lr": 0.002},
-    "biomed_clipseg": {"batch_size": 128, "lr": 0.002},
-    "biomed_clipseg_d": {"batch_size": 128, "lr": 0.002},
-    "cris": {"batch_size": 32, "lr": 0.00002},
+    "clipseg": {"batch_size": 32, "lr": 0.001},
+    "clipseg_shallow_adapter_v": {"batch_size": 32, "lr": 0.001},
+    "clipseg_shallow_adapter_vl": {"batch_size": 32, "lr": 0.001},
+    "clipseg_shallow_adapter_vlc": {"batch_size": 32, "lr": 0.001},
+    "clipseg_dense_adapter_v": {"batch_size": 32, "lr": 0.001},
+    "clipseg_dense_adapter_vl": {"batch_size": 32, "lr": 0.001},
+    "clipseg_dense_adapter_vlc": {"batch_size": 32, "lr": 0.001},
+    "san": {"batch_size": 32, "lr": 0.0003},
+    "cris": {"batch_size": 32, "lr": 0.001},
 }
 
-# Keys to extract text prompts for some radiology and non-radiology dataset
-non_rad_prompts = [f"p{i}" for i in range(10)]
-chexlocalze_prompts = [f"p{i}" for i in range(7)]
-camus_prompts = [f"p{i}" for i in range(8)]
-busi_prompts = [f"p{i}" for i in range(7)]
-
-dataset_prompts = {
-    "kvasir_polyp": non_rad_prompts,
-    "bkai_polyp": non_rad_prompts,
-    "clinicdb_polyp": non_rad_prompts,
-    "isic": non_rad_prompts,
-    "dfu": non_rad_prompts,
-    "camus": camus_prompts,
-    "busi": busi_prompts,
-    "chexlocalize": chexlocalze_prompts,
-    "pooled_polyp": non_rad_prompts,
-    "pooled_all": ["random"]
-}
+datasets = [
+    "kvasir_polyp",
+    "bkai_polyp", 
+    "clinicdb_polyp",
+    "isic",
+    "dfu",
+    "camus",
+    "busi",
+    "chexlocalize",
+]
 
 task_name: Literal["pred", "eval", "train"] = "eval"
 
+seeds = [41, 42, 43]
+
+prompt_type = "random"
 """
     DEFAULT CONFIGS BLOCK -- end
 """
--- scripts/finetune.py
+++ scripts/finetune.py
@@ -24,58 +24,58 @@ models_configs = {
 
 
 prompts = ["random"]
-dataset_prompts = {
-    "kvasir_polyp": prompts,
-    "bkai_polyp": prompts, 
-    "clinicdb_polyp": prompts,
-    "isic": prompts,
-    "dfu": prompts,
-    "camus": prompts,
-    "busi": prompts,
-    "chexlocalize": prompts,
-}
+datasets = [
+    "kvasir_polyp",
+    "bkai_polyp", 
+    "clinicdb_polyp",
+    "isic",
+    "dfu",
+    "camus",
+    "busi",
+    "chexlocalize",
+]
 
 models = [
-    "clipseg_shallow_adapter_v",
-    "clipseg_shallow_adapter_vl",
-    "clipseg_shallow_adapter_vlc",
+    "cris",
     "clipseg_dense_adapter_v",
     "clipseg_dense_adapter_vl",
     "clipseg_dense_adapter_vlc",
+    "clipseg_shallow_adapter_v",
+    "clipseg_shallow_adapter_vl",
+    "clipseg_shallow_adapter_vlc",
     "san",
     "clipseg",
-    "cris"
 ]
 
-precision="16-mixed"
-devices=[0]
-# freeze_encoder = True
-
+# prompt_type = "random"
+# precision="16-mixed"
+# devices=[0]
 
 # CUSTOM CONFIGS BLOCK -- end:
 
 
+# Loops for running the finetune experiments
 for model in models:
-    # Model specific cfgs
-    cfg = models_configs[model]
-    batch_size, lr = itemgetter("batch_size", "lr")(cfg)
-
-    for dataset, prompts in dataset_prompts.items():
-        for p in prompts:
-            # Define bash command here
+    for dataset in datasets:
+        # Model specific cfgs
+        cfg = models_configs[model]
+        batch_size, lr = itemgetter("batch_size", "lr")(cfg)
+        for seed in seeds:
+            # Define bash command here for individual experiments
             command = f"python src/train.py \
                 experiment={model}.yaml \
-                experiment_name={model}_{dataset}_{p} \
+                experiment_name={model}_{dataset}_seed_{seed} \
                 datamodule=img_txt_mask/{dataset}.yaml \
                 datamodule.batch_size={batch_size} \
                 model.optimizer.lr={lr} \
                 trainer.accelerator={accelerator} \
                 trainer.precision={precision} \
                 trainer.devices={devices} \
-                prompt_type={p} \
+                prompt_type={prompt_type} \
+                seed={seed} \
                 logger=wandb.yaml \
-                tags='[{model}, {dataset}, finetune, {p}]' \
-                output_masks_dir=output_masks/{model}/ft/{dataset}/{p}"
+                tags='[{model}, {dataset}, {prompt_type}, seed_{seed}]' \
+                output_masks_dir=output_masks/{model}/{dataset}/{prompt_type}"
 
             if debugger:
                 command = f"{command} debug=default"
@@ -83,7 +83,7 @@ for model in models:
             # Log command in terminal
             print(f"RUNNING COMMAND \n{command}")
 
-            # Run the command
+            # Run the defined command
             if os.system(command=command) != 0:
                 # Block to run after encountering error
                 print(f"!!! ERROR - COMMAND FAILED!!! \n{command}")
